26c5a6d757d5440e2b508c8b3bd9339211c1645a
Yury Popov <yuri.ppv@gmail.com>
2022-05-06 17:04:09 +0000

    Fix marks saving

--- src/dataset_creation/async_download_marks.py
+++ src/dataset_creation/async_download_marks.py
@@ -3,6 +3,8 @@ import asyncio
 import gzip
 import json
 import re
+import csv
+from itertools import chain
 
 # from tqdm.auto import tqdm
 import numpy as np
@@ -22,7 +24,7 @@ parser.add_argument(
     'work_ids',
     type=str,
     nargs='?',
-    default='data/raw/work_ids.txt',
+    default='data/raw/work_ids_cut.txt',
     help='path to work ids file',
   )
 parser.add_argument(
@@ -40,14 +42,21 @@ with open(args.work_ids, 'r') as f:
 
 def get_marks_from_html(html, work_id) -> list:
     marks_strs = html[html.find('marks = []'): html.find('</script><br><br><b>') - 1].split('\n')
-    marks_dicts = [
-      {
-        'user_id': int(re.search(pattern=r'\.userid=\d+', string=marks_str).group(0).split('=')[-1]),
-        'mark': int(re.search(pattern=r'\.mark=\d{1,2}', string=marks_str).group(0).split('=')[-1]),
-        'date': re.search(pattern=r'\.date=".*?"', string=marks_str).group(0)[7:-1],
-        'work_id': work_id
-      } for marks_str in marks_strs]
+    # mark_dicts = [
+    #   {
+    #     'user_id': int(re.search(pattern=r'\.userid=\d+', string=marks_str).group(0).split('=')[-1]),
+    #     'mark': int(re.search(pattern=r'\.mark=\d{1,2}', string=marks_str).group(0).split('=')[-1]),
+    #     'date': re.search(pattern=r'\.date=".*?"', string=marks_str).group(0)[7:-1],
+    #     'work_id': work_id
+    #   } for marks_str in marks_strs]
+    marks = tuple((
+        int(re.search(pattern=r'\.userid=\d+', string=marks_str).group(0).split('=')[-1]),
+        int(re.search(pattern=r'\.mark=\d{1,2}', string=marks_str).group(0).split('=')[-1]),
+        re.search(pattern=r'\.date=".*?"', string=marks_str).group(0)[7:-9],
+    ) for marks_str in marks_strs)
 
+    # return mark_dicts
+    return marks
 
 async def fetch(session, work_id):
     sleep_time = np.random.uniform(low=1, high=1.2)
@@ -55,10 +64,10 @@ async def fetch(session, work_id):
     url = args.query_template.format(work_id=work_id)
     async with session.get(url) as response:
         result = await response.text('utf-8')
-        # print(result[:110])
 
-        marks_dicts = get_marks_from_html(result, work_id)
-        return marks_dicts
+        mark_dicts = get_marks_from_html(result, work_id)
+        # print(mark_dicts)
+        return mark_dicts
 
     # async with asyncio.Semaphore(10):
     #     return await marks_dicts
@@ -70,7 +79,6 @@ async def fetch_with_sem(session, work_id, sem):
 
 
 async def main():
-    marks = []
     sem = asyncio.Semaphore(4)
     async with aiohttp.ClientSession() as session:
         # results = await asyncio.gather(
@@ -79,9 +87,18 @@ async def main():
         results = await tqdm_asyncio.gather(
           *[fetch_with_sem(session, work_id, sem) for work_id in work_ids]
         )
-        marks.extend(results)
-        with gzip.open(args.work_marks, 'wt') as f:
-            json.dump(marks, f)
+
+        results = list(chain.from_iterable(results)) # flattening the list
+        print(type(results))
+        print(len(results))
+        
+        # with gzip.open(args.work_marks, 'wt') as f:
+        #       (results, f)
+        with open(args.work_marks, 'w') as out:
+            csv_out=csv.writer(out)
+            csv_out.writerow(['user_id', 'mark', 'date'])
+            for row in results:
+                csv_out.writerow(row)
 
 
 asyncio.run(main())
