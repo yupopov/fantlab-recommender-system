f5c367af1bde14770c23a58df5bbc54ad373f880
Yury Popov <yuri.ppv@gmail.com>
2022-05-06 20:27:46 +0000

    (((Fix))) problems with global variables in async functions

--- src/dataset_creation/async_download_marks.py
+++ src/dataset_creation/async_download_marks.py
@@ -48,10 +48,11 @@ parser.add_argument(
 args = parser.parse_args()
 with open(args.work_ids, 'r') as f:
   work_ids = f.read().split('\n')
-
 failed_work_ids = []
 
-
+USER_AGENT = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 OPR/72.0.3815.465 (Edition Yx GX)',
+}
 
 def get_marks_from_html(html, work_id) -> list:
     marks_strs = html[html.find('marks = []'): html.find('</script><br><br><b>') - 1].split('\n')
@@ -64,6 +65,7 @@ def get_marks_from_html(html, work_id) -> list:
     #   } for marks_str in marks_strs]
     marks = tuple((
         int(re.search(pattern=r'\.userid=\d+', string=marks_str).group(0).split('=')[-1]),
+        work_id,
         int(re.search(pattern=r'\.mark=\d{1,2}', string=marks_str).group(0).split('=')[-1]),
         re.search(pattern=r'\.date=".*?"', string=marks_str).group(0)[7:-10],
     ) for marks_str in marks_strs)
@@ -75,7 +77,7 @@ async def fetch(session, work_id):
     sleep_time = np.random.uniform(low=1, high=1.2)
     await asyncio.sleep(sleep_time)
     url = args.query_template.format(work_id=work_id)
-    async with session.get(url) as response:
+    async with session.get(url, headers=USER_AGENT) as response:
         if response.status != 200:
             failed_work_ids.append(work_id)
             return
@@ -84,9 +86,6 @@ async def fetch(session, work_id):
         mark_dicts = get_marks_from_html(result, work_id)
         # print(mark_dicts)
         return mark_dicts
-
-    # async with asyncio.Semaphore(10):
-    #     return await marks_dicts
 
 
 async def fetch_with_sem(session, work_id, sem):
@@ -98,8 +97,10 @@ async def main():
     attempt_num = 1 # counting attempts to download the htmls
     sem = asyncio.Semaphore(50)
     results = []
-    while work_ids and attempt_num < 10:
-        async with aiohttp.ClientSession() as session:
+    global work_ids # ??? without this everything breaks gracefully
+    global failed_work_ids # ???? understand why this happens
+    async with aiohttp.ClientSession() as session:
+        while work_ids and attempt_num < 10:
             print(f'Starting attempt {attempt_num}...')
             attempt_results = await tqdm_asyncio.gather(
                 *[fetch_with_sem(session, work_id, sem) for work_id in work_ids],
@@ -107,22 +108,22 @@ async def main():
 
             # deleting failed responses
             attempt_results = [result for result in attempt_results if result is not None]
+            print(f'Attempt {attempt_num}: Downloaded marks for {len(attempt_results)} works, Failed for {len(failed_work_ids)} works')
+            
             attempt_results = list(chain.from_iterable(attempt_results)) # flattening the list
             results.extend(attempt_results)
-
-            print(f'Attempt {attempt_num}: Downloaded marks for {len(attempt_results)} works, Failed for {failed_work_ids} works')
 
             # trying to download failed responses again
             work_ids = failed_work_ids
             failed_work_ids = []
             attempt_num += 1
-
-            await asyncio.sleep(5)
+            if work_ids:
+                await asyncio.sleep(5)
 
         # saving results to file
         with gzip.open(args.work_marks, 'wt') as out:
             csv_out=csv.writer(out)
-            csv_out.writerow(['user_id', 'mark', 'date'])
+            csv_out.writerow(['user_id', 'work_id', 'mark', 'date'])
             for row in results:
                 csv_out.writerow(row)
         
@@ -132,5 +133,17 @@ async def main():
             work_ids = [str(work_id) for work_id in work_ids]
             with open(args.failed_work_ids, 'w') as f:
                 f.write('\n'.join(work_ids))
+
+# MWE for weird stuff we've seen above
+# This works just fine
+# async def hui():
+#     print(work_ids)
+
+# And this function can't see global variables
+# Because it modifies them
+# async def hui():
+#     print(work_ids)
+#     work_ids = []
+
 
 asyncio.run(main())
