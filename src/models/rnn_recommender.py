211de8d828c2a45441bd3862995ba49b2b2fdf96
Yury Popov <yuri.ppv@gmail.com>
2022-05-27 16:46:39 +0000

    Add activation and dropout to linear layers

--- src/models/rnn_recommender.py
+++ src/models/rnn_recommender.py
@@ -3,7 +3,7 @@ import torch
 from torch import nn
 from torch.nn.functional import sigmoid, relu, elu, tanh
 from torch.nn import Module, Embedding, LSTM, RNN, GRU, Linear, Sequential, Dropout, \
-    CrossEntropyLoss
+    CrossEntropyLoss, Sigmoid, Tanh, ReLU, ELU, BatchNorm1d
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 
@@ -20,7 +20,19 @@ cell_types = {
             "LSTM": LSTM
             }
 
+activation_types = {
+            "sigmoid": Sigmoid(),
+            "tanh": Tanh(),
+            "relu": ReLU(),
+            "elu": ELU(),
+        }
+
+
 class RecurrentLanguageModel(Module):
+    """
+    A recurrent network that tries to predict next 
+    item in a sequence.
+    """
     def __init__(self, config: dict, vocab: dict, embs: torch.Tensor):
         super().__init__()
         self.vocab = vocab
@@ -28,6 +40,8 @@ class RecurrentLanguageModel(Module):
         self.embs = Embedding.from_pretrained(
           embs, freeze=config.get('freeze_embs', True), 
           padding_idx=len(vocab)-1)
+        # By RNNDataset construction, padding is the last
+        # row in the embedding and consists of zeros
         cell_class = cell_types[config["cell_type"]]
         self.cell = cell_class(input_size=embs.size(1),
                                batch_first=True,
@@ -36,35 +50,34 @@ class RecurrentLanguageModel(Module):
                                dropout=config["cell_dropout"],
                                bidirectional=False,
                                )
-        activation_types = {
-            "sigmoid": sigmoid,
-            "tanh": tanh,
-            "relu": relu,
-            "elu": elu,
-        }
         self.out_activation = activation_types[config["out_activation"]]
         self.out_dropout = Dropout(config["out_dropout"])
-        # cur_out_size = config["hidden_size"] * config["num_layers"]
         cur_out_size = config["hidden_size"] 
+        # cur_out_size = config["hidden_size"] * config["num_layers"]
         # if config["bidirectional"]:
         #     cur_out_size *= 2
         out_layers = []
         for cur_hidden_size in config["out_sizes"]:
-            out_layers.append(Linear(cur_out_size, cur_hidden_size))
+            layer = Sequential(
+                Linear(cur_out_size, cur_hidden_size),
+                self.out_activation,
+                self.out_dropout
+            )
+            # out_layers.append(Linear(cur_out_size, cur_hidden_size))
+            out_layers.append(layer)
             cur_out_size = cur_hidden_size
         out_layers.append(Linear(cur_out_size, len(self.vocab)))
         self.out_proj = Sequential(*out_layers)
 
     def forward(self, input):
+        # input: (batch_size, max_seq_len)
         embedded = self.embs(input)
+        # embedded: (batch_size, max_seq_len, self.embs.shape[1])
         hidden_states, _ = self.cell(embedded)
-        # if isinstance(last_state, tuple):
-        #     last_state = last_state[0]
-        # last_state = last_state.transpose(0, 1)
-        # last_state = last_state.reshape(last_state.size(0), -1)
-        self.out_activation
-        self.out_dropout
-        return self.out_proj(hidden_states)
+        # hidden_states: (batch_size, max_seq_len, self.cell.hidden_size)
+        # Now pass each hidden state through a sequence
+        # of dense layers independently
+        return self.out_proj(hidden_states) # (batch_size, max_seq_len, len(self.vocab))
         # return predicts
 
 
@@ -90,6 +103,8 @@ class RecurrentRecommender(Module):
         # batch_preds: (len(user_ids), self.pred_dataset.max_seq_len, len(self.model.vocab))
         # Leave the last hidden state of the last layer
         # We use it to predict the next item in the sequence
+        # The last index in the last dimension corresponds to padding,
+        # so drop it as well
         batch_preds = batch_preds[:, -1, :-1].detach().cpu().numpy()
         # batch_preds: (len(user_ids), len(self.model.vocab))
         return batch_preds
